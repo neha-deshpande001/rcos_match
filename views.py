e4755b8e8e07bab832a6b5085103ca975666b3d5
Neha Deshpande <neha.deshpande001@gmail.com>
2021-10-19 16:00:58 -0400

    edited matching_submit; confirm button works for assigning unknown elephant sighting

--- views.py
+++ views.py
@@ -18,13 +18,11 @@ def get_individual_seek():
         if individual.individual_sighting_set.count()
     ]
 
-match_form = Match_Form()
 
 def matching(request, individual_id, match_index):
 
     # get the unknown elephant's Individual_Sighting
-    individual_sighting_unknown = get_object_or_404(
-        Individual_Sighting, pk=individual_id)
+    individual_sighting_unknown = get_object_or_404(Individual_Sighting, pk=individual_id)
 
     bbox_set = individual_sighting_unknown.sighting_bounding_box_set.all()
 
@@ -54,11 +52,6 @@ def matching(request, individual_id, match_index):
             seek_identities.append(last_individual_sighting.seek_identity)
 
     seek_identities = np.array(seek_identities,dtype=object) # convert to numpy array
-    print("gender?",seek_identities[0].gender)
-    print("age?",seek_identities[0].age)
-    print("seek?",seek_identities[0])
-    print("name?",seek_identities[0].individual_sighting.individual.name)
-    print("id?",seek_identities[0].individual_sighting.id)
     codes = np.array([np.array(code) for code in seek_identities])
 
     if 'binary' in request.GET and request.GET['binary'] == 'on':
@@ -94,14 +87,12 @@ def matching(request, individual_id, match_index):
 
 
     individual_sighting_id_match = results_list[match_index]['id']
-    individual_sighting = get_object_or_404(
-        Individual_Sighting, pk=individual_sighting_id_match)
+    individual_sighting = get_object_or_404(Individual_Sighting, pk=individual_sighting_id_match)
     individual_id_match = 0
     if individual_sighting.individual:
         individual_id_match = individual_sighting.individual.id
 
-    indiv = get_object_or_404(
-        Individual, pk=individual_id_match)
+    indiv = get_object_or_404(Individual, pk=individual_id_match)
 
     bbox_set = individual_sighting.sighting_bounding_box_set.all()
 
@@ -115,8 +106,7 @@ def matching(request, individual_id, match_index):
     }] for bbox in bbox_set}
 
     match_form = Match_Form(indiv=indiv,indiv_sight=individual_sighting_unknown)
-    print("match_form.indiv",match_form.indiv)
-    print("match_form.indiv_sight",match_form.indiv_sight)
+
     context = {
         'results_list': json.dumps(results_list), # to use in javascript
         'results': results, # to use in html
@@ -132,15 +122,76 @@ def matching(request, individual_id, match_index):
 
     return render(request, 'rcos_match/matching/index.html', context)
 
+
+
 def matching_submit(request, individual_id, match_index):
+
+    # get the unknown elephant's Individual_Sighting
+    indiv_sight = get_object_or_404(Individual_Sighting, pk=individual_id)
+
+    given_code = Seek_Identity_Form(request.GET).save(commit=False)
+
+    # get Seek_Identity of existing Individuals' most recent Individual_Sighting
+    individuals = np.array(Individual.objects.all(), dtype=object)
+    seek_identities = []
+    for i in individuals:
+        # ensure that a sighting exists for this individual
+        try:
+            last_individual_sighting = i.individual_sighting_set.latest()
+        except Individual_Sighting.DoesNotExist:
+            last_individual_sighting = None
+
+        if last_individual_sighting:
+            seek_identities.append(last_individual_sighting.seek_identity)
+
+    seek_identities = np.array(seek_identities,dtype=object) # convert to numpy array
+    
+    codes = np.array([np.array(code) for code in seek_identities])
+
+    if 'binary' in request.GET and request.GET['binary'] == 'on':
+        total_match = np.all((codes == given_code) | (
+            codes == '?') | (np.array(given_code) == '?'), axis=1)
+        seek_identities = seek_identities[total_match]
+        codes = codes[total_match]
+
+    if 'individual' in request.GET:
+        total_match = np.array([seek_identity.individual_sighting.individual is not None and
+                                seek_identity.individual_sighting.individual.pk ==
+                                int(request.GET['individual'])
+                                for seek_identity in seek_identities])
+        seek_identities = seek_identities[total_match]
+        codes = codes[total_match]
+
+    scores = np.mean(codes == given_code, axis=1) - \
+        0.4*np.mean(codes == '?', axis=1)
+
+    results = np.column_stack((scores, seek_identities))[np.argsort(-scores)]
+    results_list = list()
+    
+
+    for score, seek in results:
+        temp_dict = {'seek_code': str(seek), 'score': score}
+
+        if seek.individual_sighting.individual:
+            temp_dict['name'] = seek.individual_sighting.individual.name
+        else:
+            temp_dict['name'] = "Unknown"
+        temp_dict['id'] = seek.individual_sighting.id
+        results_list.append(temp_dict)
+
+
+    individual_sighting_id_match = results_list[match_index]['id']
+    individual_sighting = get_object_or_404(Individual_Sighting, pk=individual_sighting_id_match)
+    individual_id_match = 0
+    if individual_sighting.individual:
+        individual_id_match = individual_sighting.individual.id
+
+    indiv = get_object_or_404(Individual, pk=individual_id_match)
+
+
+
     if request.method == 'POST':
-
-        indiv = match_form.indiv
-        indiv_sight = match_form.indiv_sight
-
-        print("indiv",indiv) # indiv is None
-        print("indiv_sight",indiv_sight) # indiv_sight is None
-
+        # assign the individual sighting to the individual
         indiv_sight.individual = indiv
         indiv_sight.save()
         
@@ -150,7 +201,7 @@ def matching_submit(request, individual_id, match_index):
 
 def table(request):
     context = {
-        'tabledata':Seek_Identity.objects.all()
+        'tabledata': get_individual_seek()
     }
     return render(request,"rcos_match/table/seek_table.html",context)
 
