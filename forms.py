b3b7752a092ba4414b98e6a913f03209bc255d78
Neha Deshpande <neha.deshpande001@gmail.com>
2021-11-20 22:10:05 -0500

    Peter's security updates

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.conf import settings
-
-
-class Completed_Form(forms.Form):
-    """Form for user to select if the match is completed"""
-    completed = forms.BooleanField(required=False)
--- urls.py
+++ urls.py
@@ -5,8 +5,6 @@ from . import views
 app_name = 'rcos_match'
 
 urlpatterns = [
-    path('matching/<int:individual_id>/<int:match_index>/', views.matching, name='matching'),
-    path('matching/<int:individual_id>/<int:match_index>/submit',
-         views.matching_submit, name='matching submit'),
-    path('table/<int:individual_id>', views.table, name="seek_table")
+    path('matching/<int:pk>/<int:match_index>/', views.Match_View.as_view(), name='matching'),
+    path('table/<int:pk>', views.Table_View.as_view(), name='seek table')
 ]
